77a0edb770f7929520d67f5c76edb82f33994da8
Samuel Ko <koshunyin@gmail.com>
2023-06-18 01:52:24 +1000

    refactor

--- src/codechef/CS2023PON.java
+++ src/codechef/CS2023PON.java
@@ -1,26 +1,31 @@
 package codechef;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+// https://www.codechef.com/problems/CS2023_PON
 
 class CS2023PON {
     @SuppressWarnings("java:S106")
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
         var testCaseCount = scanner.nextInt();
-        for (var i = 0; i < testCaseCount; i++) {
-            var arraySize = scanner.nextInt();
-            var target = scanner.nextInt();
+        IntStream
+                .range(0, testCaseCount)
+                .forEach(testCase -> {
+                    var arraySize = scanner.nextInt();
+                    var target = scanner.nextInt();
+                    var arr = Arrays
+                            .stream(new Integer[arraySize])
+                            .map(x -> scanner.nextInt())
+                            .collect(Collectors.toList());
 
-            var arr = new ArrayList<Integer>();
-            for (var j = 0; j < arraySize; j++) {
-                arr.add(scanner.nextInt());
-            }
-
-            var result = handleTestCase(arr, target);
-            System.out.println(result ? "YES" : "NO");
-        }
+                    var result = handleTestCase(arr, target);
+                    System.out.println(result ? "YES" : "NO");
+                });
     }
 
     protected static boolean handleTestCase(List<Integer> arr, Integer target) {
